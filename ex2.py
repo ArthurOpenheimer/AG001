2db23827eef33cbf584c4b1cd6c28a652de2dc3e
Arthur <artopenheimervilela@gmail.com>
2023-11-25 16:34:14 -0300

    feat: add exercise 2

---
+++ ex2.py
@@ -0,0 +1,17 @@
+from sympy import Integral, Derivative, Symbol, cos
+from math import pi
+
+c = 225 % 10
+
+def v(x):
+    return 15 * 40 * pi * cos((40 * pi * x) - c)
+
+x = Symbol('x')
+
+aceleracao = Derivative(v(x), x).doit()
+deslocamento = Integral(v(x), x).doit()
+
+print('Aceleração: ', aceleracao)
+print('Deslocamento: ', deslocamento)
+print('Aceleração em t = 10: ', aceleracao.subs({x: 10}))
+
