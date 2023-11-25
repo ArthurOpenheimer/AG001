b8bdc05a2c9da4e6babbd66b61eb214237e08046
Arthur <artopenheimervilela@gmail.com>
2023-11-25 16:34:02 -0300

    fix: update exercise 1

--- ex1.py
+++ ex1.py
@@ -1,16 +1,15 @@
 from sympy import Limit, Symbol, S 
 
-matricula = 225
-c = matricula%10
+c = 225%10
 x = Symbol('x')
 
-def function(x, c):
-    return (1 + ((c + 4)/x**3))**x**3
+def function(x):
+    return (1 + ((c + 4)/x**3))**(x**3)
 
-res1 = Limit(function(x,c),x,0).doit() 
-res2 = Limit(function(x,c),x,S.Infinity).doit()
-res3 = Limit(function(x,c),x,-S.Infinity).doit()
+res1 = Limit(function(x), x, 0).doit()
+res2 = Limit(function(x), x, S.Infinity).doit()
+res3 = Limit(function(x), x, -S.Infinity).doit()
 
-print('O limite para x-->0 é de {}'.format(res1))  #resultado 1
-print('O limite para x-->+oo é de {}'.format(res2)) #resultado exp(9)
-print('O limite para x-->-oo é de {}'.format(res3)) #resultado exp(9)
+print('O limite para x-->0 é de {}'.format(res1))
+print('O limite para x-->+oo é de {}'.format(res2))
+print('O limite para x-->-oo é de {}'.format(res3))
